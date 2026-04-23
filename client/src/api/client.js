5ab406319eb1b34c93d757f1c18c853af0a15f21
sakil62 <parvezsakil259@gmail.com>
2026-04-24 00:02:44 +0530

    Added backend url to client

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // Ensure this matches your server port
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://anuprerna-task-1-7kfz.vercel.app/api'
 });
 
 /**
