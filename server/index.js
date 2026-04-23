27cb26353cf3e0b7ef49434ff94fce0759a3272a
sakil62 <parvezsakil259@gmail.com>
2026-04-24 00:00:16 +0530

    Added deployed frontend url to cors

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const apiRouter = require('./routes/api');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://anuprerna-task-1.vercel.app', 'http://localhost:5173']
+}));
 app.use(express.json());
 app.use('/api', apiRouter);
 
